6dc280e5fb198a77a5ccb853c808056d14965a0c
nakazawadesu <choconamunamu@gmail.com>
2025-08-25 18:42:45 +0900

    Delete main directory

--- main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO shop (name, category, address, description)
-VALUES ('名古屋めし太郎', '和食', '名古屋市中区', 'ひつまぶしが名物のお店');
-
-INSERT INTO shop (name, category, address, description)
-VALUES ('ナゴヤラーメン', '中華', '名古屋市中村区', '台湾ラーメンで有名');
